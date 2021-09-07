9b581d4d8fcccd030d12d6d281418e77686f5c79
Marko Miholjcic <u0984549@utah.edu>
2021-09-06 22:49:45 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/** The script to run the program on the webpage */
+
+let data = d3.csv("./data/Evaluation_Report.csv");
+
+Promise.all([data]).then(data => {
+
+    let preData = data[0];
+
+    //let bars = new visual(preData);
+
+})
