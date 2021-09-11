1ed6f4f050b184198c2c03bc88f949cbdf31d3a7
Marko Miholjcic <u0984549@utah.edu>
2021-09-11 17:10:53 -0600

    update

--- js/dataSelection.js
+++ js/dataSelection.js
@@ -9,31 +9,31 @@ class dataSelection {
 
         let demoData = d3.dataSelection("#demoButton");
 
-        demoData.on("click", function() {
-            document.getElementById("diabetesButton").style.color = "black"
-            document.getElementById("diabetesButton").style.backgroundColor = "silver"
-            that.updateData("demo");
-            that.preloaded = "demo";
-        });
+        // demoData.on("click", function() {
+        //     document.getElementById("demoButton").style.color = "black"
+        //     document.getElementById("demoButton").style.backgroundColor = "silver"
+        //     that.updateData("demo");
+        //     that.preloaded = "demo";
+        // });
 
-        let csv_file = d3.select("#csv");
-        csv_file.on("change", function () {
-            let reader = new FileReader();
+        // let csv_file = d3.select("#csv");
+        // csv_file.on("change", function () {
+        //     let reader = new FileReader();
             
-            reader.readAsText(this.files[0]);
+        //     reader.readAsText(this.files[0]);
 
-            reader.onload = function () {
-                let textData = reader.result;
-                textData = textData.split("/\r\n|\n/");
-                let customData = jQuery.csv.toObjects(textData[0]);
-                that.updateData("custom", customData);
-            }
-        });
+        //     reader.onload = function () {
+        //         let textData = reader.result;
+        //         textData = textData.split("/\r\n|\n/");
+        //         let customData = jQuery.csv.toObjects(textData[0]);
+        //         that.updateData("custom", customData);
+        //     }
+        // });
 
-        let performButton = d3.select("#performButton");
-        performButton.on("click", function () {
-            that.performAnalysis(that.data, that.numArch, that.preloaded);
-        });
+        // let performButton = d3.select("#performButton");
+        // performButton.on("click", function () {
+        //     that.performAnalysis(that.data, that.numArch, that.preloaded);
+        // });
 
     }
 
