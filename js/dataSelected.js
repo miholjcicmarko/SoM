18249fb18383dcbf86e19e9a737e0ba01454cd0d
Marko Miholjcic <u0984549@utah.edu>
2021-09-11 17:30:01 -0600

    update

---
+++ js/dataSelected.js
@@ -0,0 +1,9 @@
+class dataSelected {
+
+    constructor(data, updateData, performAnalysis) {
+        this.data = null;
+        this.updateData = updateData;
+        this.performAnalysis = performAnalysis;
+    }
+
+}
